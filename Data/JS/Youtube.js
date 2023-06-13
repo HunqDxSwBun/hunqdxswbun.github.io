e1240a5ff522be1578cebc489ced6e7f401de4e2
Chesino <117713475+HunqDxSwBun@users.noreply.github.com>
2023-06-13 17:43:22 +0700

    HunqD

--- Data/JS/Youtube.js
+++ Data/JS/Youtube.js
@@ -3,13 +3,22 @@ var isNewTabOpened = false;
 
 function chuyenDoi() {
   var inputText = document.getElementById("inputLinkYTB").value;
+  var inputTypeYTB = document.getElementById("inputTypeYTB").value;
   var resultDiv = document.getElementById("outLinkYTB");
-
+  
+  
   // Kiểm tra trường nhập liệu có rỗng hay không
   if (inputText === "") {
     resultDiv.innerHTML = "<b>Vui lòng nhập một đường dẫn YouTube hợp lệ.</b>";
     return;
   }
+
+  if (inputTypeYTB === 'mp4') {
+    var downloadType = 'button';
+  }else {
+    var downloadType = 'single';
+  }
+
   // Tạo một iframe và thiết lập thuộc tính src bằng đường dẫn URL từ API
   var iframe = document.createElement("iframe");
   iframe.id = "iframeYTB";
@@ -17,7 +26,8 @@ function chuyenDoi() {
   block.id = "Block";
   block.innerHTML = '<p>Bấm vào ô dưới đây.</p> <p>Sau đó chờ và bấm vào <b>"Download"</b> để tải về.</p>';
 
-  iframe.src = "https://convert2mp3s.com/api/single/mp3?url=" + encodeURIComponent(inputText);
+  
+  iframe.src = "https://convert2mp3s.com/api/" + encodeURIComponent(downloadType) + "/" + encodeURIComponent(inputTypeYTB) + "?url=" + encodeURIComponent(inputText);
 
   // Xóa bất kỳ nội dung cũ trong div
   resultDiv.innerHTML = "";
@@ -25,6 +35,12 @@ function chuyenDoi() {
   // Thêm iframe vào div
   resultDiv.appendChild(block);
   resultDiv.appendChild(iframe);
+
+  if (inputTypeYTB === 'mp4') {
+    document.getElementById('iframeYTB').style.height = '530px' ;
+  }else {
+    document.getElementById('iframeYTB').style.height = '220px' ;
+  }
 
 }
 
