331811c08a7ba50662094e9aaa32339c3158f214
Chesino <117713475+HunqDxSwBun@users.noreply.github.com>
2023-06-14 12:17:39 +0700

    HunqD

--- Data/JS/File.js
+++ Data/JS/File.js
@@ -1,4 +1,4 @@
-document.addEventListener('gesturestart', function(event) {
+document.addEventListener('gesturestart', function (event) {
   event.preventDefault(); // Chặn sự kiện zoom
 });
 
@@ -32,7 +32,11 @@ function openTAB(evt, TabName) {
   } else {
     body.style.overflow = "auto";
   }
-  
+
+  if (TabName !== "Story") {
+    stopAllVideos();
+  }
+
   document.getElementById(TabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
@@ -47,7 +51,7 @@ function DownloadStory() {
 
   var percent = 0;
   progressBar.style.width = percent + "%";
-  
+
   if (Dowloading.style.display === "block") {
     Dowloading.style.display = "none";
   } else {
@@ -57,18 +61,18 @@ function DownloadStory() {
   BTNDowload.style.display = "none";
 
   var percent = 0;
-  var interval = setInterval(function() {
+  var interval = setInterval(function () {
     percent += 1;
     progressBar.style.width = percent + "%";
     if (percent >= 100) {
-      
+
       clearInterval(interval);
       if (percent === 100) {
         setTimeout(() => {
           BTNDowload.style.display = "block";
         }, 2000);
       }
-      
+
     }
   }, 80);
 }
@@ -88,12 +92,12 @@ var Folderd = document.getElementById("Folder");
 var YTB = document.getElementById("YoutubeMp3");
 
 function Folder() {
-    if (Folderd.style.display === "block") {
-      Folderd.style.display = "none";
-    } else {
-      Folderd.style.display = "block";
-      YTB.style.display = "none";
-    }
+  if (Folderd.style.display === "block") {
+    Folderd.style.display = "none";
+  } else {
+    Folderd.style.display = "block";
+    YTB.style.display = "none";
+  }
 }
 
 function YoutubeMP3() {
@@ -106,11 +110,11 @@ function YoutubeMP3() {
 }
 
 function Copy(x) {
- navigator.clipboard.writeText(x);
+  navigator.clipboard.writeText(x);
   alert('Sao chép thành công');
 }
 
-var vReset = 0 ;
+var vReset = 0;
 function Reset() {
   vReset++;
   if (vReset === 2) {
@@ -128,7 +132,114 @@ function Reset() {
 var heartContainer = document.getElementById('heartContainer');
 var isAnimating = false;
 
-function toggleSize() {
-    var heart = document.querySelector('.Heart');
-    heart.classList.toggle('large');
+
+var MusicBG = document.getElementById("MusicBG");
+
+function playAudio() {
+  MusicBG.play();
 }
+
+function pauseAudio() {
+  MusicBG.pause();
+}
+
+var toggle = 0;
+function toggleSize() {
+  toggle++
+  var heart = document.querySelector('.Heart');
+  heart.classList.toggle('large');
+  if (toggle == 1) {
+    playAudio();
+  } else {
+    toggle = 0;
+    pauseAudio();
+  }
+}
+function stopAllVideos() {
+  // Tìm tất cả các phần tử video trên trang
+  const videos = document.getElementsByTagName('video');
+
+  // Dừng tất cả các video
+  for (let i = 0; i < videos.length; i++) {
+    videos[i].pause();
+  }
+}
+
+var vCoupleChat = document.getElementById('CoupleChat');
+var vWho = document.getElementById('Who');
+var vChat = document.getElementById('Chat');
+var Name = '';
+
+function CoupleChat(who, chat) {
+  vCoupleChat.style.display = "block";
+  // Kiểm tra là ai và biệt danh hay gọi
+  
+  function CheckWho() {
+    if (who === "Male") {
+      Name = 'Anh Yêu ❤';
+      vCoupleChat.style.borderColor = 'var(--Male)';
+      vWho.style.color = 'var(--Male)';
+    }
+    if (who === "Female") {
+      Name = 'Em Yêu ❤';
+      vCoupleChat.style.borderColor = 'var(--Female)';
+      vWho.style.color = 'var(--Female)';
+    }
+  }
+  CheckWho();
+
+  if (chat === undefined) {
+    // Lấy dữ liệu JSON từ đường dẫn
+  fetch('Data/Chat/Chat.json')
+  .then(response => response.json())
+  .then(jsonData => {
+    // Tìm dữ liệu phù hợp trong JSON
+    var matchingData = jsonData.filter(function (data) {
+      return data.Who === who;
+    });
+
+    if (matchingData.length > 0) {
+      var randomIndex = Math.floor(Math.random() * matchingData.length);
+      var data = matchingData[randomIndex];
+
+      setTimeout(function () {
+        CheckWho();
+        vWho.innerText = (who === "Male") ? 'Anh Yêu ❤' : 'Em Yêu ❤';
+        vChat.innerText = data.Say;
+        setTimeout(function () {
+          vWho.innerText = (who === "Male") ? 'Em Yêu ❤' : 'Anh Yêu ❤';
+          CheckWho2();
+          console.log(who);
+          vChat.innerText = data.Rep;
+          setTimeout(function () {
+            vCoupleChat.style.display = "none";
+          }, parseInt(data.tRep));
+        }, parseInt(data.tSay));
+      }, 0);
+    } else {
+      vCoupleChat.style.display = "none";
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+    vCoupleChat.style.display = "none";
+  });
+  } else {
+    vWho.innerText = Name;
+    vChat.innerText = chat;
+  }
+  
+}
+
+function CheckWho2() {
+  if (vWho.innerText === 'Anh Yêu ❤' ) {
+    vCoupleChat.style.borderColor = 'var(--Male)';
+    vWho.style.color = 'var(--Male)';
+  } else {
+    vCoupleChat.style.borderColor = 'var(--Female)';
+    vWho.style.color = 'var(--Female)';
+  }
+
+}
+
+
