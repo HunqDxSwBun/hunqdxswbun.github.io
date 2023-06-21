8b88efd6f0e72421f7d40cdaebe4c3f9ec242c46
Chesino <117713475+HunqDxSwBun@users.noreply.github.com>
2023-06-21 18:40:12 +0700

    HunqD

--- Feeds/Data/JS/Script.js
+++ Feeds/Data/JS/Script.js
@@ -2,52 +2,52 @@ const rssUrl1 = 'https://hunqdswbun.data.blog/feed/';
 const proxyUrl = 'https://api.codetabs.com/v1/proxy?quest=';
 
 fetch(proxyUrl + encodeURIComponent(rssUrl1))
-    .then(response => response.text())
-    .then(str => new DOMParser().parseFromString(str, "text/xml"))
-    .then(data => {
-        const items = data.querySelectorAll("item");
+  .then(response => response.text())
+  .then(str => new DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const items = data.querySelectorAll("item");
 
-        let Story = '';
-        items.forEach(item => {
-            const title = item.querySelector("title").textContent;
-            const encoded = item.querySelector("encoded").textContent;
-            const pubDate = item.querySelector("pubDate").textContent;
-            const creator = item.querySelector("creator").textContent;
-            const pubDateTimeStamp = Date.parse(pubDate);
-            const nowTimeStamp = Date.now();
-            const timeDiff = nowTimeStamp - pubDateTimeStamp;
+    let Story = '';
+    items.forEach(item => {
+      const title = item.querySelector("title").textContent;
+      const encoded = item.querySelector("encoded").textContent;
+      const pubDate = item.querySelector("pubDate").textContent;
+      const creator = item.querySelector("creator").textContent;
+      const pubDateTimeStamp = Date.parse(pubDate);
+      const nowTimeStamp = Date.now();
+      const timeDiff = nowTimeStamp - pubDateTimeStamp;
 
-            // Chuyển khoảng thời gian từ millisecond sang giây, phút, giờ hoặc ngày
-            const secondDiff = Math.floor(timeDiff / 1000);
-            const minuteDiff = Math.floor(timeDiff / (1000 * 60));
-            const hourDiff = Math.floor(timeDiff / (1000 * 60 * 60));
-            const dayDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-            let TimeDiff = '';
-            if (dayDiff > 0) {
-                TimeDiff = dayDiff + " ngày trước";
-            } else if (hourDiff > 0) {
-                TimeDiff = hourDiff + " giờ trước";
-            } else if (minuteDiff > 0) {
-                TimeDiff = minuteDiff + " phút trước";
-            } else {
-                TimeDiff = "Vừa xong";
-            }
+      // Chuyển khoảng thời gian từ millisecond sang giây, phút, giờ hoặc ngày
+      const secondDiff = Math.floor(timeDiff / 1000);
+      const minuteDiff = Math.floor(timeDiff / (1000 * 60));
+      const hourDiff = Math.floor(timeDiff / (1000 * 60 * 60));
+      const dayDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      let TimeDiff = '';
+      if (dayDiff > 0) {
+        TimeDiff = dayDiff + " ngày trước";
+      } else if (hourDiff > 0) {
+        TimeDiff = hourDiff + " giờ trước";
+      } else if (minuteDiff > 0) {
+        TimeDiff = minuteDiff + " phút trước";
+      } else {
+        TimeDiff = "Vừa xong";
+      }
 
-            let Author = '';
-            let srcIMG = '';
-            if (creator === 'HunqD') {
-                Author = 'Đinh Mạnh Hùng';
-                srcIMG = 'https://graph.facebook.com/100045640179308/picture?type=large&amp;access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662';
-            } else if (creator === 'hoangnguyen thythy') {
-                Author = 'Hoàng Nguyễn Thy Thy';
-                srcIMG = 'https://graph.facebook.com/100074217488487/picture?type=large&amp;access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662';
-            }
+      let Author = '';
+      let srcIMG = '';
+      if (creator === 'HunqD') {
+        Author = 'Đinh Mạnh Hùng';
+        srcIMG = 'https://graph.facebook.com/100045640179308/picture?type=large&amp;access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662';
+      } else if (creator === 'hoangnguyen thythy') {
+        Author = 'Hoàng Nguyễn Thy Thy';
+        srcIMG = 'https://graph.facebook.com/100074217488487/picture?type=large&amp;access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662';
+      }
 
-            const videoUrl = encoded.match(/src="(.*?)"/)[1];
-            const videoBlockRegex = /<figure class="wp-block-video">([\s\S]*?)<\/figure>/gm;
-            const contentWithoutVideoBlock = encoded.replace(videoBlockRegex, '');
-            
-            Story += `
+      const videoUrl = encoded.match(/src="(.*?)"/)[1];
+      const videoBlockRegex = /<figure class="wp-block-video">([\s\S]*?)<\/figure>/gm;
+      const contentWithoutVideoBlock = encoded.replace(videoBlockRegex, '');
+
+      Story += `
         <div class="Story">
           <div class="Head">
             <div class="Author">
@@ -66,6 +66,9 @@ fetch(proxyUrl + encodeURIComponent(rssUrl1))
               <div class="Func Share">
                 <i class="fa-solid fa-share-nodes"></i>
               </div>
+              <div class="Func Download">
+              <i class="fa-solid fa-arrow-down"></i>
+            </div>
             </div>
           </div>
           <div class="Body">
@@ -84,50 +87,89 @@ fetch(proxyUrl + encodeURIComponent(rssUrl1))
           </div>
         </div>
       `;
-        });
+    });
 
-        document.querySelector('#rss-feed').innerHTML = Story;
+    document.querySelector('#rss-feed').innerHTML = Story;
 
-        // Lấy tất cả các div có lớp wp-block-video
-        var videoDivs = document.querySelectorAll('.wp-block-video');
+    // Lấy tất cả các div có lớp wp-block-video
+    var videoDivs = document.querySelectorAll('.wp-block-video');
 
-        // Lặp qua từng div và tìm thẻ video trong mỗi div
-        videoDivs.forEach(function (videoDiv) {
-            var video = videoDiv.querySelector('video');
-            if (video) {
-                video.setAttribute('webkit-playsinline', '');
-                video.setAttribute('playsinline', '');
-                video.removeAttribute('controls', ''); // Thêm thuộc tính controls nếu chưa có
-            }
-        });
+    // Lặp qua từng div và tìm thẻ video trong mỗi div
+    videoDivs.forEach(function (videoDiv) {
+      var video = videoDiv.querySelector('video');
+      if (video) {
+        video.setAttribute('webkit-playsinline', '');
+        video.setAttribute('playsinline', '');
+        video.setAttribute('controls', ''); // Thêm thuộc tính controls nếu chưa có
+      }
+    });
 
-        const videos = document.querySelectorAll('video');
-        videos.forEach(video => {
-            video.addEventListener('click', function () {
-                if (video.paused) {
-                    video.play();
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+      video.addEventListener('click', function () {
+        if (video.paused) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      });
+    });
+
+    const soundFuncs = document.querySelectorAll('.Func.Sound');
+    soundFuncs.forEach(soundFunc => {
+      const video = soundFunc.closest('.Story').querySelector('video');
+      const icon = soundFunc.querySelector('i');
+      soundFunc.addEventListener('click', function () {
+        video.muted = !video.muted;
+        if (video.muted) {
+          icon.classList.replace('fa-volume-high', 'fa-volume-xmark');
+        } else {
+          icon.classList.replace('fa-volume-xmark', 'fa-volume-high');
+        }
+      });
+    });
+
+    const downloadButtons = document.querySelectorAll('.Func.Download');
+
+    downloadButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const story = button.closest('.Story');
+        const videoElement = story.querySelector('.Video video');
+        const sourceElement = videoElement.querySelector('source');
+        const videoSrc = sourceElement.src;
+
+        // Tạo một thẻ <a> để tải video
+        const downloadLink = document.createElement('a');
+        downloadLink.href = videoSrc;
+        downloadLink.download = 'video.mp4';
+        downloadLink.click();
+      });
+    });
+
+    const shareButton = document.querySelector('.Func.Share');
+    const storyDiv = document.querySelector('.Story');
+    
+    shareButton.addEventListener('click', function() {
+        html2canvas(storyDiv).then(function(canvas) {
+            canvas.toBlob(function(blob) { // Thêm phần này để tạo blob từ canvas
+                // Chia sẻ ảnh
+                if (navigator.share) {
+                    navigator.share({
+                        text: 'Chia sẻ trên iOS iPhone',
+                        files: [new File([blob], 'screenshot.png', { type: 'image/png' })],
+                    })
+                        .then(() => console.log('Chia sẻ thành công'))
+                        .catch((error) => console.error('Lỗi chia sẻ:', error));
                 } else {
-                    video.pause();
+                    // Hiển thị thông báo cho các trình duyệt không hỗ trợ API Web Share
+                    alert('Trình duyệt của bạn không hỗ trợ chức năng chia sẻ.');
                 }
-            });
+            }, 'image/png');
         });
-
-        const soundFuncs = document.querySelectorAll('.Func.Sound');
-        soundFuncs.forEach(soundFunc => {
-          const video = soundFunc.closest('.Story').querySelector('video');
-          const icon = soundFunc.querySelector('i');
-          soundFunc.addEventListener('click', function() {
-            video.muted = !video.muted;
-            if (video.muted) {
-              icon.classList.replace('fa-volume-high', 'fa-volume-xmark');
-            } else {
-              icon.classList.replace('fa-volume-xmark', 'fa-volume-high');
-            }
-          });
-        });
-        
-    })
-    .catch(error => console.log(error));
+    });
+    
+  })
+  .catch(error => console.log(error));
 
 // Lấy danh sách tất cả các thẻ video trên trang web
 var videoTags = document.querySelectorAll('video');
