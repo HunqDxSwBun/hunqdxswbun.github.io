7cdf904eb08f2f45ca6a22882a94a37c4a90194f
Chesino <117713475+HunqDxSwBun@users.noreply.github.com>
2023-06-29 01:33:40 +0700

    HunqD

---
+++ LVN/Data/JS/Script.js
@@ -0,0 +1,14 @@
+function openTAB(evt, TabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(TabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
